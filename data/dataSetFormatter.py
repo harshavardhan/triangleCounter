532aed3959bf0711864c5cb91a0a5af4bbe31458
Joshi Kosuru <joshi010198@gmail.com>
2018-04-08 05:17:22 +0530

    dataset checker updated

--- data/dataSetFormatter.py
+++ data/dataSetFormatter.py
@@ -6,8 +6,10 @@ if(len(sys.argv) != 2):
 
 firstLine = True
 dicti_rev = {}
+pair_map = {}
 counter = 0
 newFile  = open(sys.argv[1]+"updated", "w")
+lineCounter = 1
 with open(sys.argv[1], "r") as ins:
     for line in ins:
         if(firstLine):
@@ -20,12 +22,26 @@ with open(sys.argv[1], "r") as ins:
             temp = line.split()
             n1 = int(temp[0])
             n2 = int(temp[1])
+
             if(dicti_rev.get(n1) is None):
                 dicti_rev[n1] = counter
                 counter += 1
             if(dicti_rev.get(n2) is None):
                 dicti_rev[n2] = counter
                 counter += 1
-            newFile.write(str(dicti_rev[n1])+" "+str(dicti_rev[n2])+"\n")
+            if(dicti_rev[n1] < dicti_rev[n2]):
+                low = dicti_rev[n1]
+                high = dicti_rev[n2]
+            # elif(dicti_rev[n1] == dicti_rev[n2]):
+            #     print("Self edges in a node, inconsistent dataset")
+            #     print(lineCounter)
+            #     sys.exit()
+            else:
+                low = dicti_rev[n2]
+                high = dicti_rev[n1]
 
+            if pair_map.get((low, high)) is None:
+                newFile.write(str(low)+" "+str(high)+"\n")
+                pair_map[(low, high)] = 1
+        lineCounter += 1
 newFile.close()
